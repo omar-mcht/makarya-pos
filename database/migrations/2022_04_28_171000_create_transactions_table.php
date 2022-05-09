3ffeb0f560a3c144d4574583253be691a77643d5
unknown <omar.mochtr@gmail.com>
2022-05-09 22:38:39 +0700

    first upload

---
+++ database/migrations/2022_04_28_171000_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('member_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')->reference('id')->on('products');
+            $table->foreign('member_id')->reference('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ resources/views/admin/transaction/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+@section('header')
+    Transaction
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <h1>masuk</h1>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
